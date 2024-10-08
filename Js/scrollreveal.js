6a9259861a1eec484ed18f8f4c3ac2e2008ad106
Kaviselvan <101231282+kaviselvantamil@users.noreply.github.com>
2024-10-08 19:02:14 +0530

    Add files via upload

---
+++ Js/scrollreveal.js
@@ -0,0 +1,37 @@
+/*---------------Scroll Reveal------------------*/
+ScrollReveal({
+	distance: '80px',
+	duration: 1000,
+	delay: 200,
+});
+
+ScrollReveal().reveal('.section__text', { origin: 'right' ,interval: 50});
+ScrollReveal().reveal('.mypf-pic-container', { origin: 'left' ,interval: 50});
+ScrollReveal().reveal('.tab-nav-container', { origin: 'top' ,interval: 50});
+
+ScrollReveal().reveal('.left-box', { origin: 'left' });
+ScrollReveal().reveal('.right-box', { origin: 'right' });
+
+ScrollReveal().reveal('.skills-container', { 
+    origin: 'bottom',
+    interval: 200
+});
+
+ScrollReveal().reveal('.skillbox-child', { 
+    origin: 'bottom',
+    interval: 50
+});
+
+ScrollReveal().reveal('.project-container', { 
+    origin: 'bottom',
+    interval: 200
+});
+
+ScrollReveal().reveal('.project-cards', { 
+    origin: 'bottom',
+    interval: 50
+});
+
+ScrollReveal().reveal('.contact-left-container' ,{ origin: 'left' });
+ScrollReveal().reveal('.mobile-view-con-left' ,{ origin: 'top' });
+ScrollReveal().reveal('.contact-right-container', { origin: 'right' });
